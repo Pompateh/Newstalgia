94ab96bdd35b2db46d4207829ce1ee4182c001df
Joshi Naimish <joshi.naimish22@gmail.com>
2024-08-24 17:13:44 +0530

    fixed homepage

---
+++ js/header.js
@@ -0,0 +1,23 @@
+function headerResizeListener() {
+  let headerHeightOutput = document.querySelector(".header_demo");
+  let footerHeightOutput = document.querySelector(".index-footer");
+
+  function initializeheaderResize() {
+    if (headerHeightOutput) {
+      let getHeight = headerHeightOutput.offsetHeight;
+      document.documentElement.style.setProperty('--header-height', `${getHeight}px`);
+    }
+    if (footerHeightOutput) {
+      let getHeight = footerHeightOutput.offsetHeight;
+      document.documentElement.style.setProperty('--index-footer-height', `${getHeight}px`);
+    }
+  }
+
+  // Initialize on page load
+  initializeheaderResize();
+
+  // Reinitialize on window resize
+  window.addEventListener("resize", initializeheaderResize);
+}
+
+headerResizeListener();
