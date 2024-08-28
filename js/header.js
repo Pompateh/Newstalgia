d3468731dd0022b99e836bf508423907ea1ab6cc
Joshi Naimish <joshi.naimish22@gmail.com>
2024-08-28 20:45:23 +0530

    fixed issue for desktop4 and 7

--- js/header.js
+++ js/header.js
@@ -21,3 +21,9 @@ function headerResizeListener() {
 }
 
 headerResizeListener();
+
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
